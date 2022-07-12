9d36782eb89a695a8cd1610580ca7545d2296c9f
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-07-12 22:58:47 +1000

    Tests renamed

--- phyphox_test.go
+++ phyphox_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestLightSensor(t *testing.T) {
+func TestVSensor(t *testing.T) {
 	client, err := PhyphoxConnect("192.168.193.215:8080")
 	if err != nil {
 		t.Fatal(err)
@@ -29,4 +29,41 @@ func TestLightSensor(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestXYZSensor(t *testing.T) {
+	client, err := PhyphoxConnect("192.168.193.215:8080")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Stop()
+
+	magSensor, ok := client.RegisterSensor(MAGNETIC_FIELD).(XYZSensor)
+	if !ok {
+		t.Fatalf("The sensor is not a XYZSensor.")
+	}
+	magSensor.IncludeX()
+	magSensor.IncludeZ()
+
+	_, err = client.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	valX, err := magSensor.GetX()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("X: ", valX)
+
+	_, err = magSensor.GetY()
+	if err != nil {
+		fmt.Println("Y cannot be received. Correct")
+	}
+
+	valZ, err := magSensor.GetZ()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("Z: ", valZ)
 }
